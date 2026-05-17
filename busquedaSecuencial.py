d13fc6ea7fa64deef3409eb8e0f81460a43a74e1
luisdi2520 <luismurillo.ldm25@gmail.com>
2026-05-16 19:41:58 -0600

    creacion de los arichivos de los algoritmos de busqueda y ordenamiento

---
+++ busquedaSecuencial.py
@@ -0,0 +1,10 @@
+'''Busqueda secuencial.
+Entrada: lista(list), objeto(cualquier tipo comparable)
+Salida: indice donde se encontró el objeto, o -1 si no existe
+Restriccion: Funciona con listas ordenadas y desordenadas'''
+
+def busquedaSecuencial(lista, objeto):
+    for i, elemen in enumerate(lista):
+        if elemen == objeto:
+            return i
+    return -1
